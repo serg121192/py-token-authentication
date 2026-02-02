318d7d035ac367386443604141af280372e062f5
serg121192 <serg.k.ie101@gmail.com>
2026-02-02 17:39:08 +0200

    Simple fix

--- user/serializers.py
+++ user/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "password": {
                 "write_only": True,
-                "min_length": 8,
+                "min_length": 5,
             },
         }
 
